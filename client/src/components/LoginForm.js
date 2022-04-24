dfd62f53613462bddd20e24c5474502458809b82
patriciaderieux <6089242+patty345@users.noreply.github.com>
2022-04-23 20:33:14 -0500

    added logic to login form in the client side

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
+import { useMutation } from '@appollo/react-hooks';
 import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
 

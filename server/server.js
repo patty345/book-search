8b16a25f474319f53f6aabacb9fda183be7bdda2
patriciaderieux <6089242+patty345@users.noreply.github.com>
2022-04-24 19:07:43 -0500

    added the connection to the server.js file

--- server/server.js
+++ server/server.js
@@ -24,8 +24,11 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+})
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 });
